d9651452a3b264a36211dbb0d63d167849e233db
MoamenShoman <memoshoman2015@gmail.com>
2021-01-07 17:06:35 +0200

    Add project files.

---
+++ GUCera/Registeration.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registeration.aspx.cs" Inherits="GUCera.Registeration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            First name&nbsp;&nbsp; &nbsp;<asp:TextBox ID="firstName" runat="server" Width="221px"></asp:TextBox>
+            <br />
+             Last name&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="lastName" runat="server" Width="220px"></asp:TextBox>
+            <br />
+             Password&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="password" runat="server" Width="219px"></asp:TextBox>
+            <br />
+             Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<asp:TextBox ID="email" runat="server" Width="223px"></asp:TextBox>
+            <br />
+            Address&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<asp:TextBox ID="address" runat="server" Width="225px"></asp:TextBox>
+            <br />
+            Gender&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<asp:DropDownList ID="gender" runat="server" Width="230px">
+                <asp:ListItem Value="0">Female</asp:ListItem>
+                <asp:ListItem Value="1">Male</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            I am a/an&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="type" runat="server" Width="231px">
+                <asp:ListItem Value="2">Student</asp:ListItem>
+                <asp:ListItem Value="0">Instructor</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="register" runat="server" Text="Register" oncLick="register_Click"/>
+            <br />
+            <br />
+            <br />
+
+        </div> 
+    </form>
+</body>
+</html>
---
+++ GUCera/Registeration.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUCera
+{
+    public partial class Registeration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void register_Click(object sender, EventArgs e)
+        {
+            string connstr = WebConfigurationManager.ConnectionStrings["GUCera"].ToString();
+            SqlConnection conn = new SqlConnection(connstr);
+
+            string _firstName = firstName.Text;
+            string _lastName = lastName.Text;
+            string _password = password.Text;
+            string _email = email.Text;
+            string _address = address.Text;
+            bool _gender = gender.SelectedValue.Equals("1");
+            bool _type = type.SelectedValue.Equals("0"); //instrructor
+
+            if (_type)
+            {
+                SqlCommand instructorRegister = new SqlCommand("InstructorRegister", conn);
+                instructorRegister.CommandType = CommandType.StoredProcedure;
+
+                instructorRegister.Parameters.Add(new SqlParameter("@first_name", _firstName));
+                instructorRegister.Parameters.Add(new SqlParameter("@last_name", _lastName));
+                instructorRegister.Parameters.Add(new SqlParameter("@password", _password));
+                instructorRegister.Parameters.Add(new SqlParameter("@email", _email));
+                instructorRegister.Parameters.Add(new SqlParameter("@gender", _gender));
+                instructorRegister.Parameters.Add(new SqlParameter("@address", _address));
+
+                conn.Open();
+                instructorRegister.ExecuteNonQuery();
+                conn.Close();
+            }
+            else
+            {
+                SqlCommand studentRegister = new SqlCommand("studentRegister", conn);
+                studentRegister.CommandType = CommandType.StoredProcedure;
+
+                studentRegister.Parameters.Add(new SqlParameter("@first_name", _firstName));
+                studentRegister.Parameters.Add(new SqlParameter("@last_name", _lastName));
+                studentRegister.Parameters.Add(new SqlParameter("@password", _password));
+                studentRegister.Parameters.Add(new SqlParameter("@email", _email));
+                studentRegister.Parameters.Add(new SqlParameter("@gender", _gender));
+                studentRegister.Parameters.Add(new SqlParameter("@address", _address));
+
+                conn.Open();
+                studentRegister.ExecuteNonQuery();
+                conn.Close();
+            }
+            
+
+         
+        }
+    }
+}
